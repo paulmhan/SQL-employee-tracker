d89712d633bd3f1822f36c53247de54b1e991564
Paul Han <paul.myung.han@gmail.com>
2020-03-29 14:41:07 -0700

    added boilerplate and downloaded all npm packages

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse application body
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+// Import routes and give the server access to them.
+const routes = require("./controllers/catsController.js");
+
+app.use(routes);
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
