b0f440dd60517572ccf45561e7185c61e2f5a861
Paul Han <paul.myung.han@gmail.com>
2020-03-29 15:24:20 -0700

    added 4 departments to seed

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO department (name)
+VALUES ("Sales");
+INSERT INTO department (name)
+VALUES ("Finance");
+INSERT INTO department (name)
+VALUES ("Legal");
+INSERT INTO department (name)
+VALUES ("Engineering");
