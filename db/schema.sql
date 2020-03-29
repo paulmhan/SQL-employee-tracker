2340da6e77aaab9e5b15edaaaf19785d964a1544
Paul Han <paul.myung.han@gmail.com>
2020-03-29 15:12:10 -0700

    created employee_db

---
+++ db/schema.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+USE employee_db;
+
+CREATE TABLE employee (
+	id INT NOT NULL AUTO_INCREMENT,
+	first_name VARCHAR(30) NOT NULL,
+	last_name VARCHAR(30) NOT NULL,
+	role_id INT NOT NULL,
+    manager_id INT,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title varchar(30) NOT NULL,
+    salary DECIMAL(7,4) NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30),
+    PRIMARY KEY (id)
+);
+
--- routes/index.js
+++ routes/index.js
@@ -1,2 +1,6 @@
 //handling incoming routes and calling controller functions 
 //server to routes to controller to models 
+const express = require("express");
+
+const router = express.Router();
+
