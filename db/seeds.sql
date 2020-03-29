bc57c318035800c684de9917d325177158d9b1f6
Paul Han <paul.myung.han@gmail.com>
2020-03-29 16:13:14 -0700

    added 10 roles to seed

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,19 @@
 INSERT INTO department (name)
-VALUES ("Sales");
-INSERT INTO department (name)
-VALUES ("Finance");
-INSERT INTO department (name)
-VALUES ("Legal");
-INSERT INTO department (name)
-VALUES ("Engineering");
+VALUES 
+("Sales"),
+("Finance"),
+("Legal"),
+("Engineering");
+
+INSERT INTO role (title, salary, department_id)
+VALUES 
+("Sales Lead", 100000.00, 1),
+("Sales Person", 750000, 1),
+("Accountant", 125000, 2),
+("Analyst", 120000, 2),
+("Lawyer", 190000, 3),
+("Legal Team Lead", 225000, 3),
+("Software Engineer", 150000, 4),
+("Mechanical Engineer", 150000, 4),
+("Lead Engineer", 200000, 4);
+
