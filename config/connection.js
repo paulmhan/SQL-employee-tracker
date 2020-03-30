54d2bf1ad4d1e692453358285f9a2967f413f53c
Paul Han <paul.myung.han@gmail.com>
2020-03-29 17:11:00 -0700

    created connection to sql

--- config/connection.js
+++ config/connection.js
@@ -1,1 +1,22 @@
-//my sql boilerplate/connection
+//my sql boilerplate
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "employee_db"
+});
+
+// Make connection.
+connection.connect(function(err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
+  });
+  
+  // Export connection for our ORM to use.
+  module.exports = connection;
+  
