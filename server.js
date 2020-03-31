9aa311d7007b59fb01ad0480122eaa6cdad271fc
Paul Han <paul.myung.han@gmail.com>
2020-03-31 12:12:25 -0700

    created first prompt function with the first switch statement

--- server.js
+++ server.js
@@ -1,24 +1,36 @@
-const express = require("express");
+const inquirer = require("inquirer");
+const cTable = require('console.table');
 
-const PORT = process.env.PORT || 8080;
+function promptUser() {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                message: "What would you like to do?",
+                name: "action",
+                choices: [
+                    "Add Department",
+                    "Add Role",
+                    "Add Employee",
+                    "View All Departments",
+                    "View All Roles",
+                    "View All Employees",
+                    "Update Employee Role",
+                    // "Remove employee",
+                    // "View all employees by department",
+                    // "View all employees by manager",
+                    "Exit"
+                ]
+            }
+        ])
+        .then(function(answer) {
+            switch (answer.action){
+                case 'Add Department': {
+                    addDepartment();
+                    // console.log("Hello");
+                    break;
+                }
+        };
+})}
 
-const app = express();
-
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
-
-// Parse application body
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-
-// Import routes and give the server access to them.
-const routes = require("./controllers/catsController.js");
-
-app.use(routes);
-
-// Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
-});
+promptUser();
