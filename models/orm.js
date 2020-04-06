2707cb62d9d261ef8c47fcd1459922d8e9940207
Paul Han <paul.myung.han@gmail.com>
2020-04-06 16:04:54 -0700

    arrow functions in orm.js

--- models/orm.js
+++ models/orm.js
@@ -3,28 +3,28 @@ const connection = require("../config/connection.js");
 
 // Object for all our SQL statement functions.
 const orm = {
-    all: function(tableInput, cb) {
+    all: (tableInput, cb) => {
       let queryString = `SELECT * FROM ${tableInput};`;
       connection.query(queryString, function(err, res) {
         if (err) throw err;
         cb(res);
       });
     },
-    addDepartment: function(department, cb){
+    addDepartment: (department, cb) => {
         let queryString = `INSERT INTO department (name) VALUES ("${department}")`;
         connection.query(queryString, function(err, res) {
             if(err) throw err;
             cb(res);
         });
     },
-    addRole: function(title, salary, departmentNum, cb){
+    addRole: (title, salary, departmentNum, cb) => {
         let queryString = "INSERT INTO role (title, salary, department_id) VALUES (?,?,?)";
         connection.query(queryString, [title, salary, departmentNum], function(err,res){
             if (err) throw err;
             cb(res);
         })
     },
-    addEmployee: function(fName, lName, roleNum, cb){
+    addEmployee: (fName, lName, roleNum, cb) => {
         let queryString = "INSERT INTO employee (first_name, last_name, role_id) VALUES (?,?,?)"
         connection.query(queryString, [fName, lName, roleNum], function(err,res){
             if (err) throw err;
@@ -32,7 +32,7 @@ const orm = {
         })
 
     },
-    update: function(table, objColVals, condition, cb) {
+    update: (table, objColVals, condition, cb) => {
       let queryString = "UPDATE " + table;
   
       queryString += " SET ";
